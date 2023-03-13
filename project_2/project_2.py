810842fe407c0ab33491ac6571f8009c46013643
Kiryha <kiryha@gmail.com>
2023-03-13 18:20:16 -0400

    init project 2

---
+++ project_2/project_2.py
@@ -0,0 +1,4 @@
+"""
+This project involves implementing a hotel room bidding system that utilizes the chain of responsibility (COR) design pattern.
+The system allows customers to bid on available hotel rooms, and the COR pattern is used to handle the bid requests.
+"""
